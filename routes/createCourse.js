ba011b56264b72232dbe2bcd96ce4af8f92fea4c
KaiKai <1035759181@qq.com>
2019-05-10 04:56:20 +0800

    完成作业逻辑判断

--- routes/createCourse.js
+++ routes/createCourse.js
@@ -17,18 +17,42 @@ router.post('/', checkLogin, function (req, res, next) {
   const description = req.fields.description
   const price = parseInt(req.fields.price)
   const author = req.fields.author
+
   const quetion = req.fields.question
+  const quetion2 = req.fields.question2
+  const quetion3 = req.fields.question3
+  const quetion4 = req.fields.question4
+  const questionArray = [quetion, quetion2, quetion3, quetion4]
+  const quetionStrArr = questionArray.toString()
+
 
   const answer1 = req.fields.answer1
   const answer2 = req.fields.answer2
   const answer3 = req.fields.answer3
   const answer4 = req.fields.answer4
+  const answer5 = req.fields.answer5
+  const answer6 = req.fields.answer6
+  const answer7 = req.fields.answer7
+  const answer8 = req.fields.answer8
+  const answer9 = req.fields.answer9
+  const answer10 = req.fields.answer10
+  const answer11 = req.fields.answer11
+  const answer12 = req.fields.answer12
+  const answer13 = req.fields.answer13
+  const answer14 = req.fields.answer14
+  const answer15 = req.fields.answer15
+  const answer16 = req.fields.answer16
 
   const answersArray = []
-  answersArray.push(answer1, answer2, answer3, answer4)
+  answersArray.push(answer1, answer2, answer3, answer4, answer5, answer6, answer7, answer8, answer9, answer10, answer11, answer12, answer13, answer14, answer15, answer16)
   const answers = answersArray.toString()
 
   const answerRadio = req.fields.answerRadio
+  const answerRadio2 = req.fields.answerRadio2
+  const answerRadio3 = req.fields.answerRadio3
+  const answerRadio4 = req.fields.answerRadio4
+  const answerRadioArray = [answerRadio, answerRadio2, answerRadio3, answerRadio4]
+  const answerRadioStrArr = answerRadioArray.toString()
 
   const cover = req.files.cover.path.split(path.sep).pop()
 
@@ -37,7 +61,6 @@ router.post('/', checkLogin, function (req, res, next) {
   const vedio2 = req.files.vedio2.path.split(path.sep).pop()
   const vedio3 = req.files.vedio3.path.split(path.sep).pop()
   const vedio4 = req.files.vedio4.path.split(path.sep).pop()
-
   const vedioArray = [vedio1, vedio2, vedio3, vedio4]
   const vedio = vedioArray.toString()
 
@@ -79,9 +102,9 @@ router.post('/', checkLogin, function (req, res, next) {
     price: price,
     author: author,
     cover: cover,
-    taskQuetion: quetion,
+    taskQuetion: quetionStrArr,
     taskAnswer: answers,
-    answerRadio: answerRadio,
+    answerRadio: answerRadioStrArr,
     vedio: vedio
   }
   // 课程信息写入数据库
